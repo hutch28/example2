f1078d9ca10f3d59914c4cfe059cb44a6108fb31
hutch28 <hutchinsjo@gcc.edu>
2018-11-09 08:01:20 -0500

    change main

--- src/ex2/Main.java
+++ src/ex2/Main.java
@@ -3,8 +3,12 @@ package ex2;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
+		System.out.println("Hello Git2");
 
+	}
+	
+	public static void otherMethod() {
+		// do something else
 	}
 
 }
